9879c765fae8620bc6fc278a9c7374f4841f9bf0
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-11-04 10:12:11 +0100

    Update deploy script to actually work

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 rm -r _site
 bundle exec awestruct -P production --force
-bundle exec awestruct -P production --deploy
+#bundle exec awestruct -P production --deploy
+rsync -rv  --protocol=28 --delete _site/ undertow@filemgmt.jboss.org:/www_htdocs/undertow
